7290311ed786f85f064a89feee18733c8d486250
mehrab <mehrabgevorgyan@gmail.com>
2025-01-03 19:34:16 +0400

    middleware group

--- routes/web.php
+++ routes/web.php
@@ -9,25 +9,33 @@ Route::redirect('/', '/posts');
 // resource controller
 Route::resource('posts', PostController::class);
 
-// registration
-Route::get('/registration', [UserController::class, 'create'])->name('reg.create');
-Route::post('/registration', [UserController::class, 'store'])->name('reg.store');
-
-// login
-Route::get('/login', [UserController::class, 'login'])->name('login');
-Route::get('/logout', [UserController::class, 'logout'])->name('logout');
-
-// like, dislike
-Route::post('/posts/{id}/like', [PostController::class, 'postLike'])->name('like');
-Route::post('/posts/{id}/dislike', [PostController::class, 'postDislike'])->name('dislike');
-
-// emoji
-Route::post('posts/{id}/emoji', [PostController::class, 'emoji'])->name('emoji');
-
-// all posts from user, tag
+// get all posts from user, tag
 Route::get('user/{id}', [PostController::class, 'userAllPosts']);
 Route::get('tag/{id}', [PostController::class, 'tagAllPosts']);
 
-// addcomment
-Route::post('posts/{id}/addComment', [PostController::class, 'addComment'])->name('addComment');
+Route::middleware(['guest'])->group(function() {
+    // registration
+    Route::get('/registration', [UserController::class, 'create'])->middleware('guest')->name('reg.create');
+    Route::post('/registration', [UserController::class, 'store'])->middleware('guest')->name('reg.store');
+
+    // login
+    Route::get('/login', [UserController::class, 'login'])->middleware('guest')->name('login');
+});
+
+Route::middleware(['auth'])->group(function() {
+    // logout
+    Route::get('/logout', [UserController::class, 'logout'])->name('logout');
+
+    // add like, dislike
+    Route::post('/posts/{id}/like', [PostController::class, 'postLike'])->name('like');
+    Route::post('/posts/{id}/dislike', [PostController::class, 'postDislike'])->name('dislike');
+
+    // emoji
+    Route::post('posts/{id}/emoji', [PostController::class, 'emoji'])->name('emoji');
+
+    // add comment
+    Route::post('posts/{id}/addComment', [PostController::class, 'addComment'])->name('addComment');
+});
+
+
 
